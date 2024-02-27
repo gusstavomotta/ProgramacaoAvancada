2ec7be3751c5b3a3caf8f3e1c134e7610297558f
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-02-27 19:48:55 -0300

    Atualizado 19:48

--- Programa--o-Avan-ada/teste.java
+++ Programa--o-Avan-ada/teste.java
@@ -2,8 +2,29 @@
 public class teste {
 
     public static void main(String[] args) {
-        System.out.println("teste");
 
+        System.out.println("teste");
+        String string = "teste1";
+        System.out.println(retornaString(string));
+        somaValores(0, 7);
+    }
+
+    public static String retornaString(String string) {
+
+        return string;
+    }
+
+    public static float somaValores(float a, float b) {
+
+        try {
+            if (a == 0 || b == 0) {
+
+                throw new Exception("Deu misterio");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return a + b;
     }
 
 }
